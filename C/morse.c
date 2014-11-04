0a0f5c5139367715666641ebf4ecb8eeb35e610f
InputUsername <InputUsername@users.noreply.github.com>
2014-11-04 11:19:26 +0100

    Update and remove most code from morse.c
    I have the program finished on my other pc. I'll update this later. Or something.

--- C/morse.c
+++ C/morse.c
@@ -1,64 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
 
-void str_upper(char *s);
-char **encode(char *s);
-char *decode(char **s);
+#include "morse_ext.h"
 
 int main(int argc, char *argv[])
 {
-  int opt;
-  printf("MORSE ENCODER/DECODER\n");
-  printf("---------------------\n");
-  printf("What do you want to do?\n\n");
-  printf("[1] encode\n[2] decode\n");
-  
-  scanf("%d", opt);
-  if(opt < 1 || opt > 2) {
-    printf("Invalid option\n");
-    return 0;
-  }
-  
-  if(opt == 1) {
-    printf("\nInsert data to encode (some data loss may occur):\n>> ");
-    scanf("%s", insert_variable_name_here); //needs a different name, duh
-    char *enc = encode(variable_name_here);
-    
-    free(enc);
-  }
-  else if (opt == 2) {
-    printf("\nInsert data to decode:\n>> ");
-    scanf("%s", insert_other_variable_name_here); //obviously needs change
-    char *dec = decode(insert_other_variable_name_here);
-    
-    free(dec);
-  }
-  //else {
-  //  wait what
-  //}
-}
-
-void str_upper(char *s)
-{
-  int i;
-  for(i = 0; s[i] != '\0'; i++) {
-    if(s[i] >= 97 && s[i] <= 122) {
-      s[i] -= 32;
-    }
-  }
-}
-
-char **encode(char *s)
-{
-  str_upper(s);
-  char *enc = malloc(1024L * 1024L * 1024L * 1024L * 1024L); //legit! :D
-  
-  //write some useful code here
-}
-
-char *decode(char **s)
-{
-  char *dec = malloc(1024L * 1024L * 1024L * 1024L * 1024L); //legit! :D
-  
-  //write some usefull code here, too
+  //placeholder
 }
